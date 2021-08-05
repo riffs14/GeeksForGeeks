a1dd278acd8b68123408bff513d1e0ed8574b694
Rahul Kumar Chaudhary <rahul_c@students.iitmandi.ac.in>
2021-08-05 17:24:37 +0530

    script folder added

---
+++ script/Basics_Greedy_and_Bit_Manipulation/matrixMultiplication.py
@@ -0,0 +1,10 @@
+# 1: declare matirx in python
+# 2 : multiply matrix 
+# 3 :dot product and scalar procut
+# 4 : usage of numpy
+
+# 1  decalaration of matrix in python
+
+a=[[1,2],[3,5]]         # two D list
+print(a)
+print(a[1][2])
